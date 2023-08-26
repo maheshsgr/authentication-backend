e376882f64c7e947e66a0ff63d9e73e930a89563
blue <bl@ue.com>
2023-08-27 00:21:54 +0530

    minor refactoring

--- utils/SkidoMathUtil.js
+++ utils/SkidoMathUtil.js
@@ -1,26 +1,22 @@
-function GCDTwoNumbers(number1, number2) {
-  var a, b, temp;
-  var isDivisible = false;
-  if (number1 > number2) {
-    a = number1;
-    b = number2;
-  } else {
-    a = number2;
-    b = number1;
+// Function to calculate the greatest common divisor (GCD) of two numbers
+function calculateGCD(a, b) {
+  while (b !== 0) {
+    const temp = b;
+    b = a % b;
+    a = temp;
   }
-
-  while (b != 0) {
-    //50!=0 condition true
-    temp = b; // temp=50 then b=20
-    b = a % b; // b=50%30 b=20 again b= 30%20 b=10
-    a = temp; //a=10
-  }
-  console.log('GFC', a);
   return a;
 }
-function getLowestRatio(number1, number2) {
-  var GCD = GCDTwoNumbers(number1, number2);
-  return [number1 / GCD, number2 / GCD];
+
+// Function to simplify a ratio and return the lowest form
+function simplifyRatio(numerator, denominator) {
+  const gcd = calculateGCD(numerator, denominator);
+  const simplifiedNumerator = numerator / gcd;
+  const simplifiedDenominator = denominator / gcd;
+  return [simplifiedNumerator, simplifiedDenominator];
 }
-console.log('SkidoMathUtil | lowestRation ::', lowestRatio);
-module.exports = { getLowestRatio };
+
+module.exports = {
+  calculateGCD,
+  simplifyRatio,
+};
--- utils/SkidoVariantUtil.js
+++ utils/SkidoVariantUtil.js
@@ -1,17 +1,16 @@
-const { getLowestRatio } = require('./SkidoMathUtil');
+const { simplifyRatio } = require('./SkidoMathUtil');
+
+const VARIANT_A = 'variantA';
+const VARIANT_B = 'variantB';
 
 function variantCalculator(ratioNumber1, userTobeAssigned) {
-  //   console.log(
-  //     'SkidoVariantUtil | variantCalculator :: ratioNumber1, userTobeAssigned',
-  //     ratioNumber1,
-  //     userTobeAssigned
-  //   );
   if (userTobeAssigned <= ratioNumber1) {
-    return 1;
+    return VARIANT_A;
   }
 
-  return 2;
+  return VARIANT_B;
 }
+
 function getVariantForNextUser(
   ratioNumber1,
   ratioNumber2,
@@ -19,38 +18,12 @@ function getVariantForNextUser(
   totalUserLengthAlreadyAssigned,
   targetSampleSetSize
 ) {
-  const nextVariant = calculateVariantForNextUser(
-    ratioNumber1,
-    ratioNumber2,
-    varaintBAssignedCount,
-    totalUserLengthAlreadyAssigned,
-    targetSampleSetSize
-  );
-  if (nextVariant === 1) {
-    return 'variantA';
-  } else {
-    return 'variantB';
-  }
-}
-function calculateVariantForNextUser(
-  ratioNumber1,
-  ratioNumber2,
-  varaintBAssignedCount,
-  totalUserLengthAlreadyAssigned,
-  targetSampleSetSize
-) {
-  // console.log('SkidoVariantUtil', {
-  //   ratioNumber1,
-  //   ratioNumber2,
-  //   varaintBAssignedCount,
-  //   totalUserLengthAlreadyAssigned,
-  //   targetSampleSetSize,
-  // });
   if (targetSampleSetSize <= varaintBAssignedCount) {
-    return 1;
+    return VARIANT_A;
   }
-  const [minRatio1, inRatio2] = getLowestRatio(ratioNumber1, ratioNumber2);
-  var ratioSum = minRatio1 + inRatio2;
+  const [minRatio1, minRatio2] = simplifyRatio(ratioNumber1, ratioNumber2);
+  console.log('SkidoVariantUtil | minRatio1 ::', minRatio1, minRatio2);
+  var ratioSum = minRatio1 + minRatio2;
   var targetUserNumber = totalUserLengthAlreadyAssigned + 1;
 
   // for initail users
@@ -62,16 +35,6 @@ function calculateVariantForNextUser(
   var currentModeNumber = ((targetUserNumber - 1) % ratioSum) + 1;
 
   return variantCalculator(minRatio1, currentModeNumber);
-}
-var targetSampleSet = 50;
-for (var i = 0; i < 100; i++) {
-  var nextVariant = getVariantForNextUser(80, 20, i, i, targetSampleSet);
-  console.log(
-    'SkidoVariantUtil | nextVariant ::',
-    i,
-    'variant ->',
-    nextVariant
-  );
 }
 
 module.exports = { getVariantForNextUser };
